f45a539c41c60bab90dde36d714c7b3321607235
myportafoliobryantnzapp <bry.tandazo@gmail.com>
2022-09-12 06:23:14 -0500

    Sprint0

---
+++ src/routes/PublicRoute.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+
+import { Navigate } from 'react-router-dom';
+import { AuthContext } from '../components/contexts/auth/AuthContext';
+
+
+
+
+export const PublicRoute = ({ children }) => 
+{
+    const { logged } = useContext(AuthContext);
+
+    return (!logged) ? children : <Navigate to='/' />
+}
+
+
